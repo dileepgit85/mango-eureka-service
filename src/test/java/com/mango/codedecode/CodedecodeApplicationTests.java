74edba2eded51958ac22ee487a6d484bb712f32b
dileepgit85 <dileep.mavidi@gmail.com>
2024-09-22 18:10:25 +0530

    additional files commit

---
+++ src/test/java/com/mango/codedecode/CodedecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mango.codedecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodedecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
